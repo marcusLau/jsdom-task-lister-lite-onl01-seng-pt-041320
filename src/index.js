61d9a46c15c06c0f7f013356a3239bd28a63d35b
marcusLau <marcus.lau76@gmail.com>
2020-10-21 17:42:36 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+const taskForm = document.querySelector("#create-task-form")
+const list = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function (e) {
   // your code here
+  e.preventDefault() // prevents from reloading
+
+  const newTask = document.querySelector("#new-task-description").value
+
+  list.innerHTML += `
+    <li> ${newTask} </li>
+    `
 });
